995e68ed2cc7d70e00b0f1db0a46f9e5a4f4ee34
gabrielepetralia <gabri.petralia03@gmail.com>
2023-06-14 16:38:03 +0200

    make museums migration

---
+++ database/migrations/2023_06_14_142839_create_museums_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('museums', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 50);
+            $table->integer('coordinate_x');
+            $table->integer('coordinate_y');
+            $table->string('city', 100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('museums');
+    }
+};
