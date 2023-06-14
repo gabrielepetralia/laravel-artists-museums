8c36fc754b54fc52616f6be9b9a8b47e28667d2a
samuelebumbello <samuelebumbello17@gmail.com>
2023-06-14 17:30:15 +0200

    make museums blades

---
+++ resources/views/museums/edit.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.main');
+
+
+@section('content')
+<h1>Museums Edit</h1>
+@endsection
---
+++ resources/views/museums/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.main');
+
+
+@section('content')
+<h1>Museums Index</h1>
+@endsection
